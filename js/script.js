58eee7e60d1785c4a04ece4afdac43c1536d09c3
javohir-m77 <javohilegion01@gmail.com>
2023-08-07 23:26:53 +0500

    yaxshie

---
+++ js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+// alert("qonday")
+
+// const place = confirm("are u rich")
+    // console.log(typeof place)
+
+    // const currenPlace = +prompt("qattasan o", "salom")
+
+    // console.log(currenPlace + 10)
+
+// const newList = [];
+
+// newList[0] = prompt("Qaysi kitobni o'qigansiz", "");
+
+// console.log(newList)
+
+// const channelName  = prompt("What is your favourite channel?");
+
+// console.log(`Here is the link for this channel https://t.me/${channelName}`)
+
+// console.log(6 -  +'9');
+
+// console.log(6 % 1)
+
+// console.log(5+5 == 10)
+
+// const isAge = false;
+// const cosmo = true;
+// const love = false;
+
+// console.log(isAge && cosmo || !love)
+
+let name = 90;
+let name4 = 91;
+
+name++;
+name4--;
+
+console.log(name, name4)
