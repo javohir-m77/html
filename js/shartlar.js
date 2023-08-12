24980b53a348af9c464467c2ddfe7a798f69b8a6
javohir-m77 <javohilegion01@gmail.com>
2023-08-12 23:37:29 +0500

    shart_operator

---
+++ js/shartlar.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const color = prompt("Color?", "");
+
+// if(age > 28) {
+//     console.log("boevik film");
+// } else if( age > 18 && age<28) {
+//     console.log("romantik movie");
+// } else {
+//     console.log("salom")
+// }
+
+// age > 25 ? console.log("boevik film") : console.log("salom");
+
+
+switch (color) {
+    case "red":
+        console.log("toxten oka");
+    case "green":
+        console.log("bosin oka");
+    default:
+        console.log("sabr");
+}
+ 
