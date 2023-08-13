880fe911584befdea41ca675a33144955cbf7057
javohir-m77 <javohilegion01@gmail.com>
2023-08-13 12:39:06 +0500

    sikl

---
+++ js/sikl.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let startNum = -10;
+
+// while (startNum < 30) {
+//     console.log(startNum);
+//     startNum++;
+// }
+
+// do{
+//     console.log(startNum);
+//     startNum--;
+// } while(startNum)
+
+// for (startNum; startNum < 10; startNum++) {
+//     console.log(startNum);
+// }
+
+for ( let i = 0; i < 200; i++) {
+    startNum++;
+    console.log(startNum)
+}
