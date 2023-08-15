14f55fe2d214dbe3647c29315f4358eac0c9902d
javohir-m77 <javohilegion01@gmail.com>
2023-08-15 22:44:34 +0500

    funksiya:

---
+++ js/es6.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// function sayHelloWorld() {
+//     console.log("Qzumi")
+// }
+
+// sayHelloWorld();
+
+// function calc(a,b){
+//     console.log(a+b)
+// }
+
+// calc(7,9);
+
+// function sayMyName(name,surname){
+//     console.log(`My name is : ${name} My surname is ${surname}`)
+// }
+
+// sayMyName('salom', 'qiziq')
+
+// let a = 5;
+
+// function cosmo(c,b) {
+//      a = 6;
+//     console.log(a+b);
+//     console.log(a)
+// }
+// cosmo(6,9);
+// console.log(a)
+
+
+// const zb = function(a,c) {
+//     return a+c;
+
+// }
+
+// console.log(zb(9,8))
+
+const calc = (a,b) => {
+    console.log(a+b)
+}
+
+calc(9,4)
