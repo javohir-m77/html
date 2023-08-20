9e90da6093f853b22759e47bf0ae527c82616338
javohir-m77 <javohilegion01@gmail.com>
2023-08-20 20:29:38 +0500

    method

---
+++ js/method.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// const  user = "cosmo";
+// const arr = [1,2,3,4];
+
+// console.log(user.length);
+
+// console.log(arr.length) uzunlik
+
+// console.log(user.toUpperCase()) // katta harf
+// console.log(user.toLowerCase()) // kichkina harf
+
+// const greeting = "Hello Cosmo";
+
+// // console.log(greeting.indexOf("o"));
+
+// console.log(greeting.slice(1,10)); // hammasi
+// console.log(greeting.substring(9)); // minus ishlatib bolmaydi
+// console.log(greeting.substr(6,5)); // shu elementdan song n ta element chiqishi
+
+const num = 7.8;
+console.log(Math.round(num));
+
+let cosmo = "90.9pxqbgfvdc";
+console.log(parseInt(cosmo));
+console.log(parseFloat(cosmo));
